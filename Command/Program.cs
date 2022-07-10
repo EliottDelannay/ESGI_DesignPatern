fe9bc46710eadad6da3da29ca647e64771fd8f0c
EliottDelannay <eliott@delannay.fr>
2022-07-10 21:21:45 +0200

    command

---
+++ Command/Program.cs
@@ -0,0 +1,49 @@
+﻿public class Command : InterfaceCommand
+{
+    private string Action { get; set; }
+
+    public Command(string action)
+    {
+        Action = action;
+    }
+
+    public void Execute()
+    {
+        Console.WriteLine(Action);
+    }
+
+}
+
+public interface InterfaceCommand
+{
+    public void Execute();
+}
+
+
+public class Invoker
+{
+    private InterfaceCommand interfaceCommand;
+
+    public Invoker(InterfaceCommand interfaceCommand)
+    {
+        this.interfaceCommand = interfaceCommand;
+    }
+
+    public void Write()
+    {
+        interfaceCommand.Execute();
+    }
+
+}
+
+
+public class main
+{
+    public static void Main()
+    {
+        Command command = new Command("delete");
+        Invoker invoker = new Invoker(command);
+        invoker.Write();
+
+    }
+}
